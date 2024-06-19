679b1444c51426f9b5fe08567b7562e85045e755
superfra01 <151184247+superfra01@users.noreply.github.com>
2024-06-19 18:09:12 +0200

    sevlet mapping

--- src/main/java/control/SingInServlet.java
+++ src/main/java/control/SingInServlet.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import javax.sql.DataSource;
 import model.UserBean;
 import model.UserDAO;
 
+@WebServlet("/register")
 public class SingInServlet extends HttpServlet{
 
 	/**

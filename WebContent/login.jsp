2ab2bdf521ba1f35f5fadd77ddc8e3ff9097c214
Bruno Nesticò <165840945+BrunoNestico@users.noreply.github.com>
2024-06-21 12:07:33 +0200

    js loginValidation

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -7,6 +7,7 @@
     <title>Login</title>
     <link rel="shortcut icon" href="<%= request.getContextPath() %>/images/logo.png">
     <link href="<%= request.getContextPath() %>/css/login_register.css" rel="stylesheet">
+    <script src="<%= request.getContextPath() %>/scripts/loginValidation.js"></script> 
 </head>
 <body>
     <jsp:include page="header.jsp" />
@@ -15,9 +16,11 @@
         <div class="login-container">
             <h3>Un mondo ricco di spezie e di sapori travolgenti ti aspetta</h3>
             <h4>Login</h4>
-            <form action="<%= request.getContextPath() %>/login" method="post">
-                <input type="text" name="email" placeholder="email" required>
+            <form id="loginForm" action="<%= request.getContextPath() %>/login" method="post">
+                <input type="text" name="email" placeholder="Email" required>
+                <span id="errorEmail"></span>
                 <input type="password" name="password" placeholder="Password" required>
+                <span id="errorPassword"></span>
                 <button type="submit">Login</button>
             </form>
             <p>Don't have an account? <a href="<%= request.getContextPath() %>/register.jsp">Register</a></p>
---
+++ WebContent/scripts/loginValidaton.js
@@ -0,0 +1,42 @@
+const emailPattern = /^\S+@\S+\.\S+$/;
+const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+const emailErrorMessage = "Inserire un'indirizzo email valido<br/>";
+const passwordErrorMessage = "Inserire una password valida<br/>";
+
+function validate() {
+    let valid = true;
+    let form = document.getElementById("loginForm");
+
+    let spanEmail = document.getElementById("errorEmail");
+    if (!validateFormElem(form.email, emailPattern, spanEmail, emailErrorMessage)) {
+        valid = false;
+    }
+    let spanPassword = document.getElementById("errorPassword");
+    if (!validateFormElem(form.password, passwordPattern, spanPassword, passwordErrorMessage)) {
+        valid = false;
+    }
+
+    return valid;
+}
+
+function validateFormElem(formElem, pattern, span, message) {
+    if (!formElem.value.match(pattern)) {
+        formElem.classList.add("error");
+        span.innerHTML = message;
+        span.style.color = "red";
+        return false;
+    }
+    formElem.classList.remove("error");
+    span.style.color = "black";
+    span.innerHTML = "";
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").onsubmit = function(event) {
+        if (!validate()) {
+            event.preventDefault();
+        }
+    };
+});

fec26b4cb730e6fdd86eec53024cec3df74a11df
superfra01 <151184247+superfra01@users.noreply.github.com>
2024-06-24 17:03:50 +0200

    OrdiniServlet

---
+++ src/main/java/control/OrdiniServlet.java
@@ -0,0 +1,128 @@
+package control;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import model.CarrelloBean;
+import model.CarrelloDAO;
+import model.ContenenteBean;
+import model.ContenenteCarrelloBean;
+import model.ContenenteCarrelloCombinedKey;
+import model.ContenenteCarrelloDAO;
+import model.ContenenteDAO;
+import model.ImmagineProdottoBean;
+import model.ImmagineProdottoDAO;
+import model.MetodoDiPagamentoBean;
+import model.MetodoDiPagamentoDAO;
+import model.OrdineBean;
+import model.OrdineDAO;
+import model.ProdottoBean;
+import model.ProdottoDAO;
+import model.UserBean;
+
+@WebServlet("/orders")
+public class OrdiniServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 137981767037505776L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcherToOrdini = request.getRequestDispatcher("adminOrdini");
+		UserBean user = (UserBean) request.getSession().getAttribute("utente");
+		if(user!=null && user.getTipoUtente().equals("ADMIN")) {
+			OrdineDAO ordini = new OrdineDAO((DataSource)getServletContext().getAttribute("DataSource"));
+			ProdottoDAO prodotti = new ProdottoDAO((DataSource)getServletContext().getAttribute("DataSource"));
+			ImmagineProdottoDAO immagini = new ImmagineProdottoDAO((DataSource)getServletContext().getAttribute("DataSource"));
+			MetodoDiPagamentoDAO MetodiDiPagamento = new MetodoDiPagamentoDAO((DataSource)getServletContext().getAttribute("DataSource"));
+			try {
+				List<OrdineBean> OrdiniList = (List<OrdineBean>) ordini.doRetrieveAll("data");
+				request.getSession().setAttribute("adminOrdini", OrdiniList);
+				for(OrdineBean Ordine : OrdiniList) {
+					int idOrdine = Ordine.getIdOrdine();
+					MetodoDiPagamentoBean MetodoDiPagamento = MetodiDiPagamento.doRetrieveByOrderKey(idOrdine);
+					request.getSession().setAttribute("adminMetodoDiPagamento", MetodiDiPagamento);
+					ProdottoBean Prodotto = prodotti.doRetrieveByKey(idOrdine);
+					int idProdotto = Prodotto.getIdProdotto();
+					request.getSession().setAttribute("adminProdotto"+idProdotto, Prodotto);
+					
+					List<ImmagineProdottoBean> ImmagineProdottoList = (List<ImmagineProdottoBean>) immagini.doRetrieveByProductKey(idProdotto);
+					request.getSession().setAttribute("adminProdottoImmagini"+idProdotto, ImmagineProdottoList);
+					
+					
+					
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+			
+			
+			dispatcherToOrdini.forward(request, response);
+		}else
+			dispatcherToOrdini.forward(request, response);
+		
+		RequestDispatcher dispatcherToOrdine = request.getRequestDispatcher("/carrello");
+		UserBean utente = (UserBean) request.getSession().getAttribute("utente");
+		String email = utente.getEmail();
+		List<String> errors = new ArrayList<>();
+		
+		if(email == null || email.trim().isEmpty()) {
+			errors.add("richiesta invalida");
+		}
+		
+        if (!errors.isEmpty()) {
+        	request.setAttribute("errors", errors);
+        	RequestDispatcher dispatcherToHome = request.getRequestDispatcher("HomePage.jsp");
+        	dispatcherToHome.forward(request, response);
+        	return;
+        }
+		CarrelloDAO carrelli = new CarrelloDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		ContenenteCarrelloDAO ContenenteCarrelli = new ContenenteCarrelloDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		OrdineDAO ordini = new OrdineDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		ProdottoDAO prodotti = new ProdottoDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		ContenenteDAO contenenti = new ContenenteDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		MetodoDiPagamentoDAO MetodiDiPagamento = new MetodoDiPagamentoDAO((DataSource)getServletContext().getAttribute("DataSource"));
+		
+		
+		try {
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        		
+        		
+        		
+        		
+        		
+        dispatcherToOrdine.forward(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}

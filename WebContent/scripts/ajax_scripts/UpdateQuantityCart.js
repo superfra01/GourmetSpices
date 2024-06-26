60b0ef5adffd363e6edb577a1111f6d4d3f80d16
Bruno Nesticò <165840945+BrunoNestico@users.noreply.github.com>
2024-06-26 17:09:45 +0200

    Update UpdateQuantityCart.js

--- WebContent/scripts/ajax_scripts/UpdateQuantityCart.js
+++ WebContent/scripts/ajax_scripts/UpdateQuantityCart.js
@@ -13,7 +13,7 @@ function updateQuantity(productId) {
     .then(response => response.json())
     .then(data => {
         if (data.success) {
-            document.getElementById('total-cost').innerHTML = `Total Cost: <b>${data.totalCost}</b>€`;
+            document.getElementById('total-cost').innerHTML = `Total Cost: <b>${totalCost()}</b>€`;
             if (quantity == 0) {
                 document.getElementById('cart-item-' + productId).remove();
             }
@@ -51,4 +51,21 @@ function removeItem(productId) {
         console.error('Error:', error);
         alert('An error occurred while removing the item.');
     });
+}
+
+function totalCost(){
+	UserBean user = (UserBean) request.getSession().getAttribute("utente");
+	if (user != null) {
+		float totalCost = 0;
+		Integer idCarrello = (Integer) request.getSession().getAttribute("idCarrello");
+		List<ContenenteCarrelloBean> cartItems = (List<ContenenteCarrelloBean>) request.getSession().getAttribute("ContenenteCarrelloBeanList"+idCarrello.toString());
+		if (cartItems != null && !cartItems.isEmpty()) {
+		    for (ContenenteCarrelloBean item : cartItems) {
+		        ProdottoBean product = (ProdottoBean) request.getSession().getAttribute("ProdottoCarrello"+item.getIdProdotto());
+		        int quantity = item.getQuantita();
+		        totalCost += quantity * product.getPrezzo();
+		    }
+		}
+		return totalCost;
+	}
 }
